f6389a906b2ef845e610bdf37e46e5a230ca2279
LiWeiAn <vli16@student.aau.dk>
2019-04-09 12:48:43 +0200

    Add files via upload

--- leading.cpp
+++ leading.cpp
@@ -11,32 +11,34 @@ ros::Subscriber distance_sub;
 
 
 float camdist;
+double speed=0.4;
 
-void move(double speed, double distance, bool isForward);
+void move(double speedmove, double distance);
 
 void cameradistance(const std_msgs::Float32::ConstPtr & dist){
   camdist = dist->data;
 }
+
+
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "turtlesim_cleaner");
     ros::NodeHandle n;
 
    velocity_publisher = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1000);
-   distance_sub = n.subscribe("chatter", 1000, cameradistance);
+   distance_sub = n.subscribe("CamDist", 1000, cameradistance);
 
-    double speed, distance;
-    double angularspeed,angle;
-    bool isForward;
+double  distance;
+double angularspeed,angle;
 
 
   ros::Rate loop_rate(10);
     //distance = speed*time
 
-    speed = 0.01;
-    distance = 4;
-    isForward = 1;
-	move(speed, distance, isForward);
+    
+    distance = 10;
+
+	move(speed, distance);
 
 
     ros::spin();
@@ -44,44 +46,44 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void move(double speed, double distance, bool isForward){
+void move(double speedmove, double distance){
 	geometry_msgs::Twist vel_msg;
 	//set a random linear velocity in the x-axis
 
-		
-if (camdist > 1.2){
-	speed = speed - 0.01;
-}
-else{
-	speed = speed + 0.03;
-}
 
 
-	if (isForward){
-	vel_msg.linear.x =abs(speed);
-}else{
-	vel_msg.linear.x =-abs(speed);
-	vel_msg.linear.y =0;
-	vel_msg.linear.z =0;
-	//set a random angular velocity in the y-axis
-	vel_msg.angular.x = 0;
-	vel_msg.angular.y = 0;
-	vel_msg.angular.z = 0;
-}
 	double t0 = ros::Time::now().toSec();
 	double current_distance = 0.0;
-	ros::Rate loop_rate(100);
-	
-	
+	ros::Rate loop_rate(10);
 
 
 
+	while(current_distance<distance && speed < 1.5) {
+	if (camdist > 1.2){
+		speed = speed - 0.1;
+vel_msg.linear.x =abs(speed);
+	}
+	else{
+		speed = speed + 0.1;
+vel_msg.linear.x =abs(speed);
+	}
+ROS_INFO("speed %f",speed);
+ROS_INFO("speed %f",camdist);
+	velocity_publisher.publish(vel_msg);
+	double t1 = ros::Time::now().toSec();
 
-	do{	
+	current_distance = speed * (t1-t0);
 
-velocity_publisher.publish(vel_msg);
-		double t1 = ros::Time::now().toSec();
-		current_distance = speed * (t1-t0);
+	ros::spinOnce();
+	loop_rate.sleep();
+
+	}
+	/*do{	
+
+
+	velocity_publisher.publish(vel_msg);
+	double t1 = ros::Time::now().toSec();
+	current_distance = speed * (t1-t0);
 
 
 
@@ -89,11 +91,10 @@ velocity_publisher.publish(vel_msg);
 		ros::spinOnce();
 		loop_rate.sleep();
 		//cout<<(t1-t0)<<", "<<current_distance <<", "<<distance<<endl;
-	}while(current_distance<distance);
+	}while(current_distance<distance);*/
 
 
 	vel_msg.linear.x =0;
 	velocity_publisher.publish(vel_msg);
-
 
 }
