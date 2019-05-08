5305ee55ba1882762ccac913acabf02b19b5bb75
LiWeiAn <vli16@student.aau.dk>
2019-05-08 14:53:01 +0200

    Add files via upload

--- simple_navigation_goals/src/simple_navigation_goals.cpp
+++ simple_navigation_goals/src/simple_navigation_goals.cpp
@@ -5,76 +5,142 @@
 #include <dynamic_reconfigure/Reconfigure.h>
 #include <dynamic_reconfigure/Config.h>
 #include "geometry_msgs/Twist.h"
-#include "std_msgs/Float32.h"
 #include "nav_msgs/Odometry.h"
 #include "tf/tf.h"
 #include <fstream>
 #include <stdlib.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Float64.h"
+#include <simple_navigation_goals/PersonArray.h>
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Point32.h"
 
 using namespace std;
 
 
-float camdist; 
-float max_person_velocity = 0.3;
-float max_close_distance = 1.0;
-float max_far_distance = 2.0;
-
+float camdist;
+double speed=0.02;
+float goldenzone[2]={1, 1.2};	//inside the prefered distance
+double MinMax[2]={0.1, 0.4};    	//min/max speed
+double tooCFD[2]={0.6, 2};   		//too close/far min/max distance
+int tooCF[2]={0, 0};      		//too close/far attempts
 
 class SendGoal {
 
 private:
-ros::Subscriber distance_sub; //, getgoal_sub;
+ros::Subscriber distance_sub; 
 actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 move_base_msgs::MoveBaseGoal goal;
-bool isGuidingActive = false;
 dynamic_reconfigure::ReconfigureResponse srv_resp;
 
 public:
 SendGoal();
+void speedup();
+void speeddown();
+
 
 
 private:
-void cameradistance(const std_msgs::Float32::ConstPtr & dist);
+void lidardistance(const simple_navigation_goals::PersonArray::ConstPtr & dist);
 void SendGoalposition();
+
 };
 
 
 SendGoal::SendGoal() :  MoveBaseClient("move_base", true){
   ros::NodeHandle n;
-  distance_sub = n.subscribe("CamDist", 100, &SendGoal::cameradistance, this);
+  distance_sub = n.subscribe<simple_navigation_goals::PersonArray>("people_tracked", 100, &SendGoal::lidardistance,this);
 
 
 
-
-  //wait for the action server to come up
   while(!MoveBaseClient.waitForServer(ros::Duration(5.0))){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-SendGoalposition();
+	SendGoalposition();
 
-ros::Rate loop_rate(10);
 
-	while(ros::ok()){
-        ros::spinOnce();
-	loop_rate.sleep();
+
+	dynamic_reconfigure::ReconfigureResponse srv_resp;
+   	dynamic_reconfigure::ReconfigureRequest srv_req;
+	dynamic_reconfigure::DoubleParameter double_param;
+	dynamic_reconfigure::Config conf;
+	double_param.name = "max_vel_x";
+
+
+	ros::Rate rosRate(1);
+ while (ros::ok()) {
+
+
+
+
+      //while(ros::ok() && current_distance<distance ) {
+      	//double_param.value = speed;
+ /*
+      	  int count = 0;
+      	  float is, mean;
+
+          count++;
+      	  is += camdist;
+*/
+
+
+      	    if(camdist<goldenzone[1] && camdist>goldenzone[0]){//1.5-1
+      	      double_param.value = speed;
+
+		conf.doubles.push_back(double_param);
+		srv_req.config = conf;
+		ros::service::call("/move_base/TrajectoryPlannerROS/set_parameters", srv_req, srv_resp);
+
+      	    }else if(camdist < goldenzone[0]){
+      	      if(camdist < tooCFD[0] && camdist > 0){ //0.6
+      		        tooCF[0]++;
+      	     }if(tooCF[0] > 200){
+      		       speeddown();
+      		       tooCF[0] = 200;
+      		   }else {speedup();}
+
+      	    }else if(camdist > goldenzone[1] && camdist > 1){
+      	      if(camdist > tooCFD[1]){
+      		        tooCF[1]++;
+      	     }if(tooCF[1] > 2){
+      		       speeddown();
+      		       tooCF[1] = 2;
+      	     }else {speeddown();}
+
+           }/*else if (distance-current_distance < 1) {
+      	       speeddown();
+      	}mean = is/count;*/
+    
+			ROS_INFO("speed: %f cam: %f", speed, camdist);
+
+
+			
+		ros::spinOnce();
+		rosRate.sleep();
+}
+
 }
 
 
+void SendGoal::lidardistance(const simple_navigation_goals::PersonArray::ConstPtr & dist){
 
-}
 
+geometry_msgs::Point32 lidarpoint;
 
-void SendGoal::cameradistance(const std_msgs::Float32::ConstPtr & dist){
-  camdist = dist->data;
-ROS_INFO("cam: %f", camdist);
+lidarpoint.x= static_cast<float>(dist->people[0].pose.position.x);
+camdist = lidarpoint.x;
+
+ROS_INFO("data: %f", lidarpoint.x);
 }
 
 
 void SendGoal::SendGoalposition(){
 
 
-/***************Send Goal *****************/
+/***************Send Goal ******************/
 
 
 	  goal.target_pose.header.frame_id = "base_link";
@@ -88,55 +154,67 @@ void SendGoal::SendGoalposition(){
 	  ROS_INFO("Sending goal");
 	  MoveBaseClient.sendGoal(goal);
 
-	if(MoveBaseClient.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-	 ROS_INFO("Hooray, the base moved 1 meter forward");
-	else
-	 ROS_INFO("The base failed to move forward 1 meter for some reason");
 
 
 /*****************Speed adjust******************/
 
-		   	dynamic_reconfigure::ReconfigureRequest srv_req;
-    			dynamic_reconfigure::DoubleParameter double_param;
-    			dynamic_reconfigure::Config conf; 
-			double_param.name = "max_vel_x";
+}
+
+void SendGoal::speedup(){
+dynamic_reconfigure::ReconfigureResponse srv_resp;
+dynamic_reconfigure::ReconfigureRequest srv_req;
+dynamic_reconfigure::DoubleParameter double_param;
+dynamic_reconfigure::Config conf;
+double_param.name = "max_vel_x";
+
+
+if (speed >= MinMax[1]){
+  speed = MinMax[1];
+  double_param.value = speed;
+			
+  }else {
+    speed = speed + 0.1;
+    double_param.value = speed;
+
+  }
 			conf.doubles.push_back(double_param);
-
-
-		if(camdist < max_close_distance && max_person_velocity<0.4){
-
-			max_person_velocity = max_person_velocity + 0.1;
-
-			double_param.value = max_person_velocity;
-
-
-
-			}
- 		if(camdist > max_far_distance && max_person_velocity>0.1){
-		
-			max_person_velocity = max_person_velocity - 0.1;
-
-
-	
-			double_param.value = max_person_velocity;
-
-			}
-
 			srv_req.config = conf;
 			ros::service::call("/move_base/TrajectoryPlannerROS/set_parameters", srv_req, srv_resp);
 
-
-
 }
 
+
+
+void SendGoal::speeddown(){
+dynamic_reconfigure::ReconfigureResponse srv_resp;
+dynamic_reconfigure::ReconfigureRequest srv_req;
+dynamic_reconfigure::DoubleParameter double_param;
+dynamic_reconfigure::Config conf;
+double_param.name = "max_vel_x";
+
+
+  if (speed <= MinMax[0]){
+    speed = MinMax[0];
+    double_param.value = speed;
+			
+  }else {
+    speed = speed - 0.1;
+    double_param.value = speed;
+
+	}
+			conf.doubles.push_back(double_param);			
+			srv_req.config = conf;
+			ros::service::call("/move_base/TrajectoryPlannerROS/set_parameters", srv_req, srv_resp);
+
+}
 
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "simple_navigation_goals");
-SendGoal now;
+  SendGoal now;
 
-ros::spin();
+
+
  return 0;
 }
-
 
